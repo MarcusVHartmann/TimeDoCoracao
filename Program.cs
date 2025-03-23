802669e061f52b5cc21d18387b6203d129cbf384
MarcusVHart <haykirengeral@gmail.com>
2025-03-23 20:13:16 -0300

    Criação de sistema que exibe time de futebol com cores estilizadas de Background e Texto.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.BackgroundColor = ConsoleColor.White;
+
+Console.Write("T");
+
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+Console.Write("O");
+
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.BackgroundColor = ConsoleColor.White;
+
+Console.Write("T");
+
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+Console.Write("T");
+
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.BackgroundColor = ConsoleColor.White;
+
+Console.Write("E");
+
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+Console.Write("N");
+
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.BackgroundColor = ConsoleColor.White;
+
+Console.Write("H");
+
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+Console.Write("A");
+
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.BackgroundColor = ConsoleColor.White;
+
+Console.Write("M");
+
+
+
+Console.ResetColor();
